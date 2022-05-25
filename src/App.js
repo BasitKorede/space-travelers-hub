b19ba681a59c213a8d89f90669c96e158634e434
Lucas Shangrita <oustanilokman@gmail.com>
2022-05-25 13:14:55 +0300

    Add basic structurte for rockets

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
+import Rockets from './pages/Rockets';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route
+          exact="true"
+          path="/"
+          element={<Rockets />}
+        />
+      </Routes>
     </div>
   );
 }
--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getRockets } from '../redux/rockets';
+import Rocket from './Rocket';
 
-const Rocket = (props) => {
-	const {rocket} = props;
-	return (
-		<div className="rocket d-flex ">
-			<img />
-			<div>
+const Rockets = () => {
+	const rockets = useSelector((state) => state.rockets);
+	const dispatch = useDispatch;
 
-			</div>
+	useEffect(() => {
+    dispatch(getRockets());
+  }, [dispatch]);
+
+	return(
+		<div className="rockets d-col-flex">
+			{ rockets.length > 0 ? rockets.map((rocket) => (
+			<Rocket key={rocket.id} rocket={rocket} /> )) : '' }
 		</div>
 
 	);
+
 };
 
-export default Rocket;
+
+
+export default Rockets;
