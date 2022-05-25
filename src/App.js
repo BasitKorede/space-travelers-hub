ca8fce5f394fe6e6de4f08d89e0d55d8196af50f
Lucas Shangrita <oustanilokman@gmail.com>
2022-05-25 17:00:35 +0300

    Display rocket detail

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Header from './components/Header';
 import Rockets from './pages/Rockets';
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <Routes>
         <Route
           exact="true"
