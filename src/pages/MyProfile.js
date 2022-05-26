1c3ca5ecab4698cba90120324db9ad3259a03bd4
Lucas Shangrita <oustanilokman@gmail.com>
2022-05-26 22:10:32 +0300

    Update cancel rocket booking reducer

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const myprofile = () => {
-  <div className="profile-container" />;
+  <div className="profile-container">
+    
+  </div>;
 };
 
 export default myprofile;
