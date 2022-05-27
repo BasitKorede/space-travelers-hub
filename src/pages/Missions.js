44a5439f61a7688e42de37768f1f4ce43648fcd9
Basit Arigbabuwo <arigbabuwobasitkorede6@gmail.com>
2022-05-27 10:34:52 +0100

    Add Mission data

--- src/pages/Missions.js
+++ src/pages/Missions.js
@@ -1,4 +1,29 @@
-const Missions = () => (
-    <h1>Missions</h1>
+import React from 'react';
+import Table from 'react-bootstrap/Table';
+import { useSelector } from 'react-redux';
+
+export default function MissionsList() {
+  const missions = useSelector((state) => state.missionsReducer);
+
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Mission</th>
+          <th>Discription</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {missions.map((mission) => (
+          <tr key={mission.mission_id}>
+            <td>{mission.mission_name}</td>
+            <td>{mission.description}</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
   );
-  export default Missions;
+}
